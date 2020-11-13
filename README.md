67cc20639a15d8dbd5e284c18a781c6a14550369
otus <otusex@gmail.com>
2020-11-14 01:02:31 +0200

    Fin commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Parse access log nginx
+
+log_analysis.sh - скрипт парсит логи nignx
+access.log - лог
+
+
+запуск скрипта:
+```bash
+bash ./log_analysis.sh test@mail.ru access.log
+```
+
---
+++ log_analysis.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+
+PID_THIS_PROCCES="$$"
+PID_FILE_THIS_PROCCES="pidfile.lock"
+FILE_FIRST_LINE="last_line"
+FILE_INFO="info.txt"
+PID_RROM_FILE=""
+TIME_START=""
+TIME_END=""
+ACCESS_LOG=""
+EMAIL=""
+LAST_LINE=""
+FIRST_LINE=""
+
+echo "checking if a proccess exists..."
+sleep 3
+if [[ -f "$PID_FILE_THIS_PROCCES"  ]]; then
+ echo "Proccess is running ..."
+ exit 1
+fi
+
+echo "checking command args..."
+sleep 3
+if [[ $1 == "" ]]; then
+ echo "Input email..."
+ exit 1
+fi 
+
+if [[ $2 == "" ]]; then
+ echo "Input access.log ..."
+ exit 1
+fi
+
+EMAIL=$1
+ACCESS_LOG=$2
+
+echo "$$" > $PID_FILE_THIS_PROCCES
+
+sig_trap () {
+ trap "rm -f $PID_FILE_THIS_PROCCES info.txt; exit 1" SIGINT
+ trap "rm -f $PID_FILE_THIS_PROCCES" EXIT
+}
+
+
+echo "Start proccess..."
+sleep 2
+
+LAST_LINE=$(wc -l  $ACCESS_LOG | awk '{print $1}')
+
+if [[ -f  $FILE_FIRST_LINE && -n $(cat last_line) ]]; then 
+FIRST_LINE=$(cat last_line)  
+else 
+FIRST_LINE="1" 
+fi
+
+echo "Parse TIME_START and TIME_END..."
+sleep 3
+TIME_START=$(cat $ACCESS_LOG | sed -n "$FIRST_LINE","$LAST_LINE"p | awk '{print $4 $5}' | sed 's/\[//; s/\]//' | head -n 1)
+TIME_END=$(cat $ACCESS_LOG |   sed -n "$FIRST_LINE","$LAST_LINE"p | awk '{print $4 $5}' | sed 's/\[//; s/\]//' | tail -n 1)
+ 
+echo -e "Period: $TIME_START - $TIME_END \n" > $FILE_INFO
+
+echo "Parse logs..."
+sleep 3
+echo 'Number of requests:  IP:' >> $FILE_INFO
+cat $ACCESS_LOG | awk '{print $1}' | sort | uniq -c | sed -e "s/^ *//g" | sed -e "s/ /       /g" | sort -rn | head  >> $FILE_INFO
+echo -e '\nNumber of requests:  Requested page:' >> $FILE_INFO
+cat $ACCESS_LOG | awk '{print $7}' | sort | uniq -c | sed -e "s/^ *//g" | sed -e "s/ /       /g" | sort -rn | head  >> $FILE_INFO
+echo -e '\nNumber of requests:  Response code:' >> $FILE_INFO
+cat $ACCESS_LOG | awk '{print $9}' | sort | uniq -c | sed -e "s/^ *//g" | sed -e "s/ /       /g" | sort -rn | head  >> $FILE_INFO
+
+
+echo "Send info to email: $EMAIL ..."
+sleep 3
+sendmail $EMAIL < $FILE_INFO
+
+sig_trap
+
+echo "Write last line to file..."
+sleep 3
+echo $LAST_LINE > $FILE_FIRST_LINE
+
+rm -f $PID_FILE_THIS_PROCCES $FILE_INFO
+exit 0
+
